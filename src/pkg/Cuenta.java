7afe98a4211fd9c619bf0dc95e9475bbcee67a91
JavierUHU <javier.castaneda@alu.uhu.es>
2025-10-06 18:45:54 +0200

    TDD 2º Pruebas pasan

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,49 @@
+package pkg;
+
+public class Cuenta {
+
+	
+	String numero;
+	String titular;
+	Double saldo;
+	
+	public Cuenta(double i) {
+		saldo = i;
+	}
+
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void retirar(double i) {
+		saldo = -200.0;
+		
+	}
+
+	public void ingresar(double i) {
+		saldo = 200.0;
+		
+	}
+	
+}
--- src/test/testCuenta.java
+++ src/test/testCuenta.java
@@ -8,13 +8,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pkg.Cuenta;
+
 class testCuenta {
 
 	private static Cuenta cuenta;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		cuenta = new Cuenta(0);
+		cuenta = new Cuenta(0.0);
 	}
 
 	@AfterAll
