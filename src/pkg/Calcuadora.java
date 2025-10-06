92b1446c66f772917a0c34f5f35314fd4d83cf64
JavierUHU <javier.castaneda@alu.uhu.es>
2025-10-06 18:29:00 +0200

    TDD 1º Paso. Implementar Pruebas

---
+++ src/pkg/Calcuadora.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Calcuadora {
+
+	public static Integer suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer multiplica(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
